e0fc13e998e5cd95671d271eecdb52d7dd97e5c3
Eric Moore <ericmoore0709@gmail.com>
2024-04-17 12:45:39 -0700

    Added starting templates and routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask, render_template
-from models import db, connect_db
+from flask import Flask, render_template, redirect
+from models import User, db, connect_db
 import os
 from dotenv import load_dotenv
 
@@ -16,6 +16,50 @@ with app.app_context():
     connect_db(app)
     db.create_all()
 
+
 @app.get('/')
 def index():
-    return render_template('index.html', title='Index')
+    """Redirect to list of users."""
+    return redirect('/users')
+
+
+@app.get('/users')
+def getUsers():
+    """get list of users"""
+    return render_template('listUsers.html', users=[])
+
+
+@app.get('/users/new')
+def displayNewUserForm():
+    """show user add form"""
+    return render_template('newUser.html', user=User())
+
+
+@app.post('/users/new')
+def processNewUserForm():
+    """process add form, redirect to /users"""
+    return redirect('/users')
+
+
+@app.get('/users/[int:user_id]')
+def getUserInfo(user_id):
+    """display user info"""
+    return render_template('userDetails.html', user=User())
+
+
+@app.get('/users/[user_id]/edit')
+def getUserEditForm(user_id):
+    """display user edit form"""
+    return render_template('editUser.html', user=User())
+
+
+@app.post('/users/[user_id]/edit')
+def processUserEditForm(user_id):
+    """process user edit form, redirect to /users"""
+    return redirect('/users')
+
+
+@app.post('/users/[user_id]/delete')
+def processDeleteUser(user_id):
+    """delete user (redirect to /users)"""
+    return redirect('/users')
